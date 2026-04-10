3484d2bdc9f70b1282c75fcd8f6534d9d915ab28
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-04-10 09:48:52 +0800

    finished header, not responsive

--- main.js
+++ main.js
@@ -2,3 +2,19 @@ const button=document.querySelector(".searchButton")
 button.addEventListener("click",()=>{
     alert("Search?")
 })
+const notify=document.querySelector(".notifications")
+notify.addEventListener("click",()=>{
+    alert("Notification")
+})
+const newButton=document.querySelector(".newButton")
+newButton.addEventListener("click",()=>{
+    alert("New")
+})
+const uploadButton=document.querySelector(".uploadButton")
+uploadButton.addEventListener("click",()=>{
+    alert("Upload")
+})
+const shareButton=document.querySelector(".shareButton")
+shareButton.addEventListener("click",()=>{
+    alert("Share")
+})
